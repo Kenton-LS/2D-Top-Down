2f9e1ffbb491e36a2e2f5d21843f1dbf40243182
Kenton <kentonlesliesmith@gmail.com>
2019-10-26 15:35:32 +0200

    EnemyAI
    Test subject MeleeUnit now moves toward the placeholder player. Placeholder player can be moved using IJKL, not interfering with the WASD camera.

---
+++ UNITY/Assets/Script/MeleeUnit.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeUnit : MonoBehaviour
+{
+    private Vector3 Player;
+    private Vector2 PlayerDirection;
+    private float xDif;
+    private float yDif;
+    private float speed;
+
+    private void Start()
+    {
+        speed = 0.005f;
+    }
+
+    void Update()
+    {
+        Player = GameObject.Find("Player").transform.position;
+
+        xDif = Player.x - transform.position.x;
+        yDif = Player.y - transform.position.y;
+
+        PlayerDirection = new Vector2(xDif, yDif);
+
+        transform.Translate(PlayerDirection * speed);
+    }
+}
